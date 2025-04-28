b6945385a3155248dec322ea0ad1cff986bc130f
b8llacorrea <artemisfall8@gmail.com>
2025-04-28 17:51:43 -0300

    classe pessoa

--- app/src/main/java/ifsc/poo/App.java
+++ app/src/main/java/ifsc/poo/App.java
@@ -3,8 +3,25 @@
  */
 package ifsc.poo;
 
+// A aplicação (App.java) será usada para instanciar objetos das classes e testar suas
+//funcionalidades. Crie um metodo estático para executar o teste de cada classe e invoque-
+//os na main.
+
 public class App {
+
+
+
+
     public static void main(String[] args) {
+
+        // Objetos lampada - ver estados
+        Lampada abajur = new Lampada(true);
+        Lampada lamp2 = new Lampada(false);
+
+        System.out.println("O abajur " + "está " + abajur.verEstado() + " e a lampada 2 " + lamp2 + " está " + lamp2.verEstado());
+
+
+
 
     }
 }
--- app/src/main/java/ifsc/poo/Pessoa.java
+++ app/src/main/java/ifsc/poo/Pessoa.java
@@ -1,4 +1,44 @@
 package ifsc.poo;
 
 public class Pessoa {
+
+    private String nome;
+    private int idade;
+
+    // construtores diferentes.
+    public Pessoa(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    // pessoa - nome
+    public Pessoa(String nome) {
+        this.nome = nome;
+    }
+
+    // pessoa - idade
+    public Pessoa(int idade) {
+        this.idade = idade;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    // me retorna o nome
+    public String getNome() {
+        return nome;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void felizAniversario() {
+        idade = idade + 1;
+    }
 }
