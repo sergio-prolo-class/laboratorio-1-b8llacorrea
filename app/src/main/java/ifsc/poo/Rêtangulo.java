d3e754477667bcc3b1d40d6538af6e44e15f204b
b8llacorrea <artemisfall8@gmail.com>
2025-05-05 18:23:59 -0300

    produto

--- app/src/main/java/ifsc/poo/Rêtangulo.java
+++ app/src/main/java/ifsc/poo/Rêtangulo.java
@@ -1,10 +1,14 @@
 package ifsc.poo;
 
+// Nenhum objeto Retangulo pode ser criado sem altura e largura definidas.
+
 public class Rêtangulo {
 
     // Crie um retângulo que possua formas de calcular sua área e perímetro.
     private float largura;
     private float altura;
+    private float Area;
+    private float Perimetro;
 
     public void setAltura(float altura) {
         this.altura = altura;
@@ -14,5 +18,11 @@ public class Rêtangulo {
         this.largura = largura;
     }
 
-    
+    public float getPerimetro() {
+        return Perimetro;
+    }
+
+    public float getArea() {
+        return Area;
+    }
 }
