1516430693927e12446566a5039a39ec1bb6b76f
b8llacorrea <artemisfall8@gmail.com>
2025-05-05 18:09:34 -0300

    retangulo andamento

--- app/src/main/java/ifsc/poo/Rêtangulo.java
+++ app/src/main/java/ifsc/poo/Rêtangulo.java
@@ -1,4 +1,18 @@
 package ifsc.poo;
 
 public class Rêtangulo {
+
+    // Crie um retângulo que possua formas de calcular sua área e perímetro.
+    private float largura;
+    private float altura;
+
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+
+    public void setLargura(float largura) {
+        this.largura = largura;
+    }
+
+    
 }
