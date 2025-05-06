7051ce2467d016d0f2200934a3e40dcb2b5ce048
b8llacorrea <artemisfall8@gmail>
2025-05-05 21:59:16 -0300

    falta produto e navio

--- app/src/main/java/ifsc/poo/Retangulo.java
+++ app/src/main/java/ifsc/poo/Retangulo.java
@@ -5,14 +5,30 @@ package ifsc.poo;
 public class Retangulo {
 
     // Crie um retângulo que possua formas de calcular sua área e perímetro.
-    
+
+    // ESTATICOS
+    private static Retangulo retanguloMaior;
+
+    // Atributos
     private float largura;
     private float altura;
 
     // Construtores
     public Retangulo(float largura, float altura) {
+        // valor padrão
+        if (largura <= 0) largura = 1;
+        if (altura <= 0) altura = 1;
         this.largura = largura;
         this.altura = altura;
+        if (retanguloMaior == null
+                || this.getArea() / this.getPerimetro()
+                > retanguloMaior.getArea() / retanguloMaior.getPerimetro()) {
+                    retanguloMaior = this ; // o objeto que estou adicionando é o maior.
+        }
+    }
+
+    public static Retangulo getRetanguloMaior() {
+        return retanguloMaior;
     }
 
     public void setAltura(float altura) {
